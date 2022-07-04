583e7ca6a18511265b3ff08061cace4aa0e10728
davidflo7 <108668208+davidflo7@users.noreply.github.com>
2022-07-04 12:56:45 +0200

    Add files via upload

---
+++ test 0.py
@@ -0,0 +1,21 @@
+esercizioesercizio = 15
+print (esercizioesercizio)
+
+
+
+if(esercizioesercizio == True):
+  X1= "cappello"
+  if(esercizioesercizio == 50):
+        X1= "cappello"
+else:
+    x1= "CAPPELLO"
+
+
+
+if(esercizionumerotre == false):
+  X1="finestra"
+  if(esercizionumerotre == 0):
+    else:
+      X1= "finestra"
+
+    
